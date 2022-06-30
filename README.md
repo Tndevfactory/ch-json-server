48ea9f11dc7eb4af0e7d81f681e67293a4394ff7
tndevfactory <tndevfactory@gmail.com>
2022-06-30 01:37:31 +0100

    simple without express

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ch-json-server
+
 utility to mock api fastly based on json data
+
+# author tndev-art.com
+
+run locally on port 5200
---
+++ db.js
@@ -0,0 +1,11 @@
+const cnrps_assures = require("./data/cnrps/assures.json");
+const cnrps_pensions = require("./data/cnrps/pensions.json");
+const cnss_carrieres = require("./data/cnss/carrieres.json");
+const steg_affaires = require("./data/steg/affaires.json");
+
+module.exports = {
+  cnrps_assures,
+  cnrps_pensions,
+  cnss_carrieres,
+  steg_affaires,
+};
